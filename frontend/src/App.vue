c0cf5bc27d00750275915644b4cd2b21799eba69
Sergio Herrero Fernández <sergio.herrero.fernandez@msspain.com>
2026-02-27 08:22:28 +0000

    pruebas

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const message = ref("Cargando...")
+
+onMounted(async () => {
+  const res = await fetch("http://localhost:8000/api/health")
+  const data = await res.json()
+  message.value = data.status
+})
+</script>
+
+<template>
+  <h1>Frontend funcionando</h1>
+  <p>Backend responde: {{ message }}</p>
+</template>
